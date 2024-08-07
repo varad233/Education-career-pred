2fd42ecaec07d319481392779817a158104185ed
varad233 <136105601+varad233@users.noreply.github.com>
2024-08-07 23:06:58 +0530

    open port error handling

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ def pred():
         return render_template('results.html', recommendations=recommendations)
     return render_template('home.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
